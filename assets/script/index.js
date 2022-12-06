ad2562483aebbb590984e71ee7e533cba137c79e
inder <cheema.inder10@gmail.com>
2022-12-06 14:04:06 -0600

    bugs fixed

--- assets/script/index.js
+++ assets/script/index.js
@@ -23,6 +23,8 @@ const showInfo = select('.info');
 const game = select('.game');
 const mesg = select('.mesg');
 const startInfo = select('.start-info');
+const round = select('.round');
+
 
 const onHits = new Audio('./assets/audio/hits.wav');
 onHits.type = 'audio/wav';
@@ -49,9 +51,10 @@ const gameWords2 = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'build
 'keyboard', 'window']
 
 let gameWords = [...gameWords2];
+input.disabled = true;
 
 function mytimer() {
-    let totalTime = 98;
+    let totalTime = 5;
 let countDown = setInterval(function(){
   if(totalTime <= 0 ){
     gameLevel.pause();
@@ -96,6 +99,7 @@ onEvent('keyup', input, function() {
 
 
 onEvent('click' , btn2,  function() {
+  input.disabled = false;
     input.focus();
     words.style.visibility = 'visible';
     btn2.style.animationPlayState = 'paused';
@@ -109,12 +113,14 @@ onEvent('click' , btn2,  function() {
 onEvent('click' , btn,  function() {
     gameWords = [...gameWords2];
     getWord(gameWords);
+    round.style.display = 'inline-block';
     mesg.style.visibility = 'visible';
     gameIsOver.pause();
     gameLoad.play();
     mesg.innerText = 'Get Ready, Game is loading'
     gameOver.style.visibility = 'hidden';
     setTimeout(() => {
+      round.style.display = 'none';
         gameLevel.play();
         input.value = '';
         hits = 0;
